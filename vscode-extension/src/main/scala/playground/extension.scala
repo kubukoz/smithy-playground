d496efc5ace5c56d73ddd35b535f7bfa6af5c590
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-04 23:46:48 +0100

    grammar

--- vscode-extension/src/main/scala/playground/extension.scala
+++ vscode-extension/src/main/scala/playground/extension.scala
@@ -3,6 +3,7 @@ package playground
 import cats.effect.IO
 import cats.effect.kernel.Deferred
 import cats.effect.unsafe.implicits._
+import cats.implicits._
 import com.disneystreaming.demo.smithy.CreateHeroOutput
 import com.disneystreaming.demo.smithy.CreateSubscriptionOutput
 import com.disneystreaming.demo.smithy.DemoService
@@ -12,6 +13,9 @@ import com.disneystreaming.demo.smithy.Subscription
 import playground.Runner
 import smithy4s.http4s.SimpleRestJsonBuilder
 import typings.vscode.anon.Dispose
+import typings.vscode.mod
+import typings.vscode.mod.Diagnostic
+import typings.vscode.mod.DiagnosticSeverity
 import typings.vscode.mod.Disposable
 import typings.vscode.mod.DocumentFormattingEditProvider
 import typings.vscode.mod.ExtensionContext
@@ -19,8 +23,11 @@ import typings.vscode.mod.OutputChannel
 import typings.vscode.mod.commands
 import typings.vscode.mod.languages
 import typings.vscode.mod.window
+import typings.vscode.mod.workspace
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
+import cats.parse.Parser.Expectation.InRange
 
 object extension {
   val chan: OutputChannel = window.createOutputChannel("Smithy Playground")
@@ -46,6 +53,8 @@ object extension {
     }
 
   implicit def disposableToDispose(d: Disposable): Dispose = Dispose(() => d.dispose())
+
+  val errors = languages.createDiagnosticCollection()
 
   @JSExportTopLevel("activate")
   def activate(
@@ -73,6 +82,43 @@ object extension {
             format.perform(doc)
           },
         ),
+        workspace
+          .onDidSaveTextDocument
+          .apply(
+            doc => {
+
+              val parsed = SmithyQLParser.parser(Tokens.idTokens).parseAll(doc.getText())
+
+              parsed match {
+                case Right(_) => errors.clear()
+
+                case Left(e) =>
+                  val pos = doc.positionAt(e.failedAtOffset.toDouble)
+                  val range = new mod.Range(pos, doc.lineAt(doc.lineCount - 1).range.end)
+
+                  errors
+                    .set(
+                      doc.uri,
+                      js.Array(
+                        new Diagnostic(
+                          range,
+                          "Parsing failure: expected one of " + e
+                            .expected
+                            .map {
+                              case InRange(_, lower, upper) if lower == upper => lower.toString
+                              case InRange(_, lower, upper)                   => s"$lower-$upper"
+                              case msg                                        => msg.toString()
+                            }
+                            .mkString_(", "),
+                          DiagnosticSeverity.Error,
+                        )
+                      ),
+                    )
+              }
+            },
+            (),
+            (),
+          ),
       )
 
     val _ = window.showInformationMessage(
