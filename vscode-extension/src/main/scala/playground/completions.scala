b5abe6c477f85bbbe2d2b508347a7c6647305b92
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-09 04:18:17 +0100

    Add http/externalDocs detail

--- vscode-extension/src/main/scala/playground/completions.scala
+++ vscode-extension/src/main/scala/playground/completions.scala
@@ -1,14 +1,17 @@
 package playground
 
+import cats.implicits._
 import playground.smithyql.SmithyQLParser
 import playground.smithyql.WithSource
 import playground.smithyql.WithSource.StructThing
 import smithy.api.Documentation
+import smithy.api.Http
 import smithy4s.Service
 import typings.vscode.mod
 
 import types._
 import util.chaining._
+import smithy.api.ExternalDocumentation
 
 object completions {
 
@@ -19,7 +22,17 @@ object completions {
       .endpoints
       .map { e =>
         new mod.CompletionItem(e.name + ": Foo => Bar", mod.CompletionItemKind.Function).tap(
-          _.detail = e.hints.get(Documentation).map(_.value).getOrElse(""): String
+          _.detail = List(
+            e.hints.get(Http).map { http =>
+              show"HTTP ${http.method.value} ${http.uri.value} "
+            },
+            e.hints.get(Documentation).map(_.value),
+            e.hints.get(ExternalDocumentation).map(_.value).map {
+              _.map { case (k, v) => show"""${k.value}: ${v.value}""" }.mkString("\n")
+            },
+          ).flatten
+            .map(_ + " ") // workaround for concatenation in the shortened view
+            .mkString("\n\n")
         )
       }
 
