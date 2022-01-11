dac7a7ac9c26fd92ad8370f2b7ce32464f22a547
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-11 03:06:45 +0100

    Fix my own compilation lol

--- vscode-extension/src/main/scala/playground/client.scala
+++ vscode-extension/src/main/scala/playground/client.scala
@@ -1,16 +1,17 @@
 package playground
 
+import cats.effect.Resource
+import cats.effect.kernel.Async
+import cats.implicits._
 import demo.smithy.CreateHeroOutput
 import demo.smithy.CreateSubscriptionOutput
 import demo.smithy.DemoService
+import demo.smithy.GetPowersOutput
 import demo.smithy.Hero
 import demo.smithy.Subscription
 import org.http4s.client.Client
 import org.http4s.ember.client.EmberClientBuilder
 import smithy4s.http4s.SimpleRestJsonBuilder
-import cats.effect.Resource
-import cats.effect.kernel.Async
-import cats.implicits._
 
 object client {
 
