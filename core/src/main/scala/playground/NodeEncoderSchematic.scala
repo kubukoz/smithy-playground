7af204b5fb730add89bbb0e156d1fbd34aff99eb
Jakub Kozłowski <kubukoz@gmail.com>
2022-04-12 01:04:19 +0200

    Encode documents and others

--- core/src/main/scala/playground/NodeEncoderSchematic.scala
+++ core/src/main/scala/playground/NodeEncoderSchematic.scala
@@ -1,7 +1,14 @@
 package playground
 
+import cats.Contravariant
 import cats.Id
+import cats.implicits._
+import cats.tagless.Derive
+import playground.smithyql.BooleanLiteral
 import playground.smithyql.InputNode
+import playground.smithyql.IntLiteral
+import playground.smithyql.Listed
+import playground.smithyql.StringLiteral
 import playground.smithyql.Struct
 import schematic.Alt
 import schematic.ByteArray
@@ -13,13 +20,19 @@ import smithy4s.Timestamp
 import sourcecode.Enclosing
 
 import java.util.UUID
-import playground.smithyql.StringLiteral
-import playground.smithyql.IntLiteral
-import playground.smithyql.Listed
-import playground.smithyql.BooleanLiteral
+import smithy4s.Document.DArray
+import smithy4s.Document.DBoolean
+import smithy4s.Document.DNumber
+import smithy4s.Document.DNull
+import smithy4s.Document.DString
+import smithy4s.Document.DObject
 
 trait NodeEncoder[A] {
   def toNode(a: A): InputNode[Id]
+}
+
+object NodeEncoder {
+  implicit val contravariant: Contravariant[NodeEncoder] = Derive.contravariant
 }
 
 object NodeEncoderSchematic extends Schematic[NodeEncoder] {
@@ -27,71 +40,75 @@ object NodeEncoderSchematic extends Schematic[NodeEncoder] {
   def unsupported[A](implicit sc: Enclosing): NodeEncoder[A] =
     v => throw new Exception(s"Unsupported operation: ${sc.value} for value $v")
 
-  def short: NodeEncoder[Short] = unsupported
+  val short: NodeEncoder[Short] = unsupported
 
-  def int: NodeEncoder[Int] = IntLiteral(_)
+  val int: NodeEncoder[Int] = IntLiteral(_)
 
-  def long: NodeEncoder[Long] = l => IntLiteral(l.toInt) // todo this wraps!
+  val long: NodeEncoder[Long] = l => IntLiteral(l.toInt) // todo this wraps!
 
-  def double: NodeEncoder[Double] = unsupported
+  val double: NodeEncoder[Double] = unsupported
 
-  def float: NodeEncoder[Float] = unsupported
+  val float: NodeEncoder[Float] = unsupported
 
-  def bigint: NodeEncoder[BigInt] = unsupported
+  val bigint: NodeEncoder[BigInt] = unsupported
 
-  def bigdecimal: NodeEncoder[BigDecimal] = unsupported
+  val bigdecimal: NodeEncoder[BigDecimal] = unsupported
 
-  def string: NodeEncoder[String] = StringLiteral(_)
+  val string: NodeEncoder[String] = StringLiteral(_)
 
-  def boolean: NodeEncoder[Boolean] = b => BooleanLiteral(b)
+  val boolean: NodeEncoder[Boolean] = BooleanLiteral(_)
 
-  def uuid: NodeEncoder[UUID] = unsupported
+  val uuid: NodeEncoder[UUID] = unsupported
 
-  def byte: NodeEncoder[Byte] = unsupported
+  val byte: NodeEncoder[Byte] = unsupported
 
-  def bytes: NodeEncoder[ByteArray] =
+  val bytes: NodeEncoder[ByteArray] =
     bytes => StringLiteral(bytes.toString()) // todo this only works for UTF-8 text
 
-  def unit: NodeEncoder[Unit] = unsupported
+  // todo: inconsistent with decoder (takes everything)
+  val unit: NodeEncoder[Unit] = struct(Vector())(_ => ())
 
   def list[S](fs: NodeEncoder[S]): NodeEncoder[List[S]] = elems => Listed[Id](elems.map(fs.toNode))
 
   def set[S](fs: NodeEncoder[S]): NodeEncoder[Set[S]] = unsupported
 
-  def vector[S](fs: NodeEncoder[S]): NodeEncoder[Vector[S]] = unsupported
+  def vector[S](fs: NodeEncoder[S]): NodeEncoder[Vector[S]] = list(fs).contramap(_.toList)
 
   def map[K, V](fk: NodeEncoder[K], fv: NodeEncoder[V]): NodeEncoder[Map[K, V]] = unsupported
+
+  private def obj(
+    values: List[(String, InputNode[Id])]
+  ): Struct[Id] = Struct[Id](
+    Struct
+      .Fields
+      .fromSeq[Id](values.map(_.leftMap(Struct.Key(_))))
+  )
 
   def struct[S](
     fields: Vector[Field[NodeEncoder, S, _]]
   )(
     const: Vector[Any] => S
   ): NodeEncoder[S] =
-    s =>
-      Struct[Id] {
+    s => {
 
-        def go[A](
-          f: Field[NodeEncoder, S, A]
-        ) = f.fold(new Field.Folder[NodeEncoder, S, Option[(String, InputNode[Id])]] {
-          def onRequired[F](
-            label: String,
-            instance: NodeEncoder[F],
-            get: S => F,
-          ): Option[(String, InputNode[Id])] = Some(label -> instance.toNode(get(s)))
+      def go[A](
+        f: Field[NodeEncoder, S, A]
+      ) = f.fold(new Field.Folder[NodeEncoder, S, Option[(String, InputNode[Id])]] {
+        def onRequired[F](
+          label: String,
+          instance: NodeEncoder[F],
+          get: S => F,
+        ): Option[(String, InputNode[Id])] = Some(label -> instance.toNode(get(s)))
 
-          def onOptional[F](
-            label: String,
-            instance: NodeEncoder[F],
-            get: S => Option[F],
-          ): Option[(String, InputNode[Id])] = get(s).map(f => label -> instance.toNode(f))
-        })
+        def onOptional[F](
+          label: String,
+          instance: NodeEncoder[F],
+          get: S => Option[F],
+        ): Option[(String, InputNode[Id])] = get(s).map(f => label -> instance.toNode(f))
+      })
 
-        Struct.Fields.fromSeq {
-          fields.flatMap {
-            go(_).map { case (s, v) => Struct.Key(s) -> v }
-          }
-        }
-      }
+      obj(fields.flatMap(go(_)).toList)
+    }
 
   def union[S](
     first: Alt[NodeEncoder, S, _],
@@ -123,10 +140,27 @@ object NodeEncoderSchematic extends Schematic[NodeEncoder] {
   ): NodeEncoder[B] = b => f.toNode(from(b))
 
   // todo support formats
-  def timestamp: NodeEncoder[Timestamp] = ts => string.toNode(ts.toString())
+  val timestamp: NodeEncoder[Timestamp] = ts => string.toNode(ts.toString())
 
+  // todo
   def withHints[A](fa: NodeEncoder[A], hints: Hints): NodeEncoder[A] = fa
 
-  def document: NodeEncoder[Document] = unsupported
+  val document: NodeEncoder[Document] =
+    doc =>
+      doc match {
+        case DArray(value)   => list(document).toNode(value.toList)
+        case DBoolean(value) => boolean.toNode(value)
+        case DNumber(value) =>
+          if (value.isValidInt)
+            int.toNode(value.toInt)
+          else
+            // todo other numbers
+            bigdecimal.toNode(value)
+        case DNull =>
+          // todo nul???
+          unsupported[Null].toNode(null)
+        case DString(value) => string.toNode(value)
+        case DObject(value) => obj(value.toList.map(_.map(document.toNode)))
+      }
 
 }
