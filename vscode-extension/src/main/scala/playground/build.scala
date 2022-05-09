57a7e7ba5a394ae1f87baeb958d48fd72ac2b415
Jakub Kozłowski <kubukoz@gmail.com>
2022-05-09 15:04:53 +0200

    Use smithy4sVersion again

--- vscode-extension/src/main/scala/playground/build.scala
+++ vscode-extension/src/main/scala/playground/build.scala
@@ -115,8 +115,7 @@ object build {
         debug.timed("dump-model") {
           nodeChildProcessMod.execSync(
             // todo: pass version from workspace config, default from sbt-buildinfo
-            // todo: restore dynamic version
-            ("cs" :: "launch" :: "com.disneystreaming.smithy4s:smithy4s-codegen-cli_2.13:0.12.14" :: "--" :: args)
+            ("cs" :: "launch" :: s"com.disneystreaming.smithy4s:smithy4s-codegen-cli_2.13:${BuildInfo.smithy4sVersion}" :: "--" :: args)
               .mkString(" ")
           )
         }
