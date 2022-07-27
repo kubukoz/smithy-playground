a134f67520ffe3bde5384b63f92572948b358eb0
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-27 04:49:06 +0200

    add todo

--- core/src/main/scala/playground/TextDocumentManager.scala
+++ core/src/main/scala/playground/TextDocumentManager.scala
@@ -2,6 +2,7 @@ package playground
 
 trait TextDocumentManager[F[_]] {
   def put(uri: String, text: String): F[Unit]
+  // todo: extract to separate trait? (read-only is the major usecase)
   def get(uri: String): F[String]
   def remove(uri: String): F[Unit]
 }
