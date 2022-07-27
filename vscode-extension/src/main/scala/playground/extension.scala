91499a5160dba21305d693cc7d9988642d67c2f9
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-27 04:42:43 +0200

    cleanup

--- vscode-extension/src/main/scala/playground/extension.scala
+++ vscode-extension/src/main/scala/playground/extension.scala
@@ -1,5 +1,7 @@
 package playground
 
+import org.scalablytyped.runtime.StObject
+import typings.vscode.anon.Dispose
 import typings.vscode.mod
 import typings.vscode.mod.ExtensionContext
 import typings.vscode.mod.OutputChannel
@@ -7,7 +9,39 @@ import typings.vscode.mod.window
 import typings.vscodeLanguageclient.clientMod.LanguageClientOptions
 import typings.vscodeLanguageserverProtocol.protocolMod
 
+import scala.annotation.nowarn
 import scala.scalajs.js.annotation.JSExportTopLevel
+
+import scalajs.js
+
+@js.native
+// "dead code"
+@nowarn()
+trait ServerOptions extends StObject {
+  var command: String = js.native
+}
+
+object ServerOptions {
+
+  @scala.inline
+  def apply(command: String): ServerOptions = {
+    val __obj = js.Dynamic.literal(command = command.asInstanceOf[js.Any])
+    __obj.asInstanceOf[ServerOptions]
+  }
+
+}
+
+@js.native
+@js.annotation.JSImport("vscode-languageclient/node", "LanguageClient")
+@nowarn("cat=unused")
+class LanguageClient(
+  id: String,
+  name: String,
+  serverOptions: ServerOptions,
+  clientOptions: LanguageClientOptions,
+) extends js.Object {
+  def start(): Dispose = js.native
+}
 
 object extension {
   private val chan: OutputChannel = window.createOutputChannel("Smithy Playground", "smithyql")
