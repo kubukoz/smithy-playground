0ffbf26d0aaf1930682a28264112e43cf7407365
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-16 15:43:10 +0200

    remove redundant parameter

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -28,7 +28,6 @@ export function activate(context: ExtensionContext) {
         // "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,quiet=y,address=5010",
         `${serverArtifact}:${serverVersion}`,
         "--ttl",
-        "1h",
         coursierTTL,
       ],
     },
