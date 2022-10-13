02943aa8baf74e39ff5a3019ccc852cffc2b3c80
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-14 00:55:16 +0200

    rename, add flag

--- update-libs.sh
+++ update-libs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-function build_grammar() {
+set -e
+
+function update_grammar() {
   OUT_DIR="core/src/main/resources/$2"
   OUT="$OUT_DIR/libtree-sitter-smithyql.$3"
   mkdir -p "$OUT_DIR"
@@ -8,7 +10,7 @@ function build_grammar() {
   chmod +w "$OUT"
 }
 
-build_grammar x86_64-darwin darwin-x86_64 dylib
-build_grammar aarch64-darwin darwin-aarch64 dylib
-build_grammar x86_64-linux linux-x86_64 so
-build_grammar aarch64-linux linux-aarch64 so
+update_grammar x86_64-darwin darwin-x86_64 dylib
+update_grammar aarch64-darwin darwin-aarch64 dylib
+update_grammar x86_64-linux linux-x86_64 so
+update_grammar aarch64-linux linux-aarch64 so
