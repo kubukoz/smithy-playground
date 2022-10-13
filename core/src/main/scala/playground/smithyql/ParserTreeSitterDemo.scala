5a046293586a5cc9c240adf6550ced3ee6dab6e0
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-14 01:15:39 +0200

    Remove boilerplate

---
+++ core/src/main/scala/playground/smithyql/ParserTreeSitterDemo.scala
@@ -0,0 +1,19 @@
+package playground.smithyql
+
+import org.polyvariant.treesitter4s.bindings.TreeSitterInstance
+
+object ParserTreeSitterDemo extends App {
+
+  def parse(s: String) = {
+    val p = TreeSitterInstance.make(SmithyQLLanguageBindings.SmithyQL)
+    val tree = p.parse(s)
+    println(tree.rootNode.get.fields.keySet)
+    // OpDecoders.op.decode(tree.rootNode.get, s)
+  }
+
+  println(
+    parse(
+      "use service a.b#C\n helalsdfhl //a\n{ hello = 42, foo = 50, x = { y = \"hello\"} , z = null, aa = [10, true, false, null]}"
+    )
+  )
+}
