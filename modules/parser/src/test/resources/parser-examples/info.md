f147d106876d4dc2958760ff18cd015ee579d5eb
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-15 18:59:26 +0200

    Move to file-based parser tests

---
+++ modules/parser/src/test/resources/parser-examples/info.md
@@ -0,0 +1,5 @@
+Files in this directory are generated tests. Each directory is a test:
+
+- the directory name is the test name
+- `input.smithyql-test` contains the test's input
+- `output.json` contains the expected result of parsing the input as SmithyQL and rendering it as JSON.
--- modules/parser/src/test/scala/playground/smithyql/parser/ParserTests.scala
+++ modules/parser/src/test/scala/playground/smithyql/parser/ParserTests.scala
@@ -1,394 +1,53 @@
 package playground.smithyql.parser
 
-import cats.Id
-import weaver._
-import cats.implicits._
 import cats.effect.IO
-import playground.smithyql.parser.SmithyQLParser
-import playground.smithyql.Diffs._
+import cats.implicits._
+import fs2.io.file.Files
+import fs2.io.file.Path
+import io.circe.Codec
 import playground.smithyql._
+import playground.smithyql.parser.SmithyQLParser
+import weaver._
+
+import java.nio.file.Paths
 import playground.Assertions._
+import Diffs._
 
 object ParserTests extends SimpleIOSuite {
 
-  def parsingTest(
-    name: TestName,
-    input: String,
-  )(
-    expected: Query[Id]
-  )(
-    implicit loc: SourceLocation
-  ): Unit =
-    pureTest(name) {
-      SmithyQLParser.parse(input) match {
-        case Left(e)  => failure(s"Parsing failed: ${e.msg}")
-        case Right(v) => assert(v == expected)
-      }
-    }
+  implicit val queryWS: Codec[Query[WithSource]] = {
+    import io.circe.generic.auto._
 
-  def parsingTestFull(
-    name: TestName,
-    input: String,
-  )(
-    expected: Query[WithSource]
-  )(
-    implicit loc: SourceLocation
-  ): Unit =
-    test(name) { (_, l) =>
+    io.circe.generic.semiauto.deriveCodec
+  }
+
+  private def readText(path: Path) =
+    Files[IO]
+      .readAll(path)
+      .through(fs2.text.utf8.decode)
+      .compile
+      .string
+
+  val baseDir = Paths.get(getClass().getResource("/parser-examples").getFile())
+
+  baseDir.toFile.list().filter(baseDir.resolve(_).toFile().isDirectory()).foreach { testName =>
+    val testBase = Path.fromNioPath(baseDir) / testName
+
+    test(testName) { (_, l) =>
       implicit val log = l
-      SmithyQLParser.parseFull(input) match {
-        case Left(e)  => IO(failure(s"Parsing failed: ${e.msg}"))
-        case Right(v) => assertNoDiff(v, expected)
-      }
+
+      val inputIO = readText(testBase / "input.smithyql-test")
+
+      val outputIO = readText(testBase / "output.json")
+        .flatMap(io.circe.parser.decode[Query[WithSource]](_).liftTo[IO])
+
+      (inputIO, outputIO).mapN { (input, expected) =>
+        SmithyQLParser.parseFull(input) match {
+          case Left(e)  => failure(s"Parsing failed: ${e.msg}").pure[IO]
+          case Right(v) => assertNoDiff(v, expected)
+        }
+      }.flatten
     }
-
-  import DSL._
-
-  parsingTestFull("simple call", "hello {}")(
-    Query[WithSource](
-      useClause = WithSource.liftValue(None),
-      operationName = WithSource
-        .liftValue(
-          QueryOperationName(
-            identifier = None,
-            operationName = WithSource
-              .liftValue(OperationName[WithSource]("hello"))
-              .withRange(SourceRange(Position(0), Position(5))),
-          )
-        )
-        .withRange(SourceRange(Position(0), Position(5))),
-      input = WithSource
-        .liftValue(
-          Struct(
-            WithSource
-              .liftValue(Struct.Fields[WithSource](Nil))
-              .withRange(SourceRange.empty(Position(7)))
-          )
-        )
-        .withRange(SourceRange.empty(Position(7))),
-    )
-  )
-
-  parsingTestFull("simple call, dense", "hello{}")(
-    Query[WithSource](
-      useClause = WithSource.liftValue(None),
-      operationName = WithSource
-        .liftValue(
-          QueryOperationName(
-            identifier = None,
-            operationName = WithSource
-              .liftValue(OperationName[WithSource]("hello"))
-              .withRange(SourceRange(Position(0), Position(5))),
-          )
-        )
-        .withRange(SourceRange(Position(0), Position(5))),
-      input = WithSource
-        .liftValue(
-          Struct(
-            WithSource
-              .liftValue(Struct.Fields[WithSource](Nil))
-              .withRange(SourceRange.empty(Position(6)))
-          )
-        )
-        .withRange(SourceRange(Position(6), Position(6))),
-    )
-  )
-
-  parsingTest("simple call, space in object", "hello{ }")("hello".call())
-  parsingTest("simple call, trailing/leading space", " hello{} ")("hello".call())
-  parsingTest("simple call, sparse", " hello { } ")("hello".call())
-  parsingTest("simple call, sparse with underscore", " hello_world { } ")("hello_world".call())
-
-  parsingTest("simple call with float", "hello { a = 21.37 }")("hello".call("a" -> 21.37f))
-  parsingTest(
-    "simple call with double",
-    s"hello { a = ${Double.MaxValue.toString} }",
-  )(
-    "hello".call(
-      "a" -> Double.MaxValue
-    )
-  )
-
-  parsingTest("use service", "use service com.example#Demo hello {}")(
-    "hello".call().useService("com", "example")("Demo")
-  )
-
-  parsingTest("use service with numbers", "use service com1.example2#Demo3 hello {}")(
-    "hello".call().useService("com1", "example2")("Demo3")
-  )
-
-  parsingTest("use service with whitespace", "use service com1 . example2 # Demo3 hello {}")(
-    "hello".call().useService("com1", "example2")("Demo3")
-  )
-
-  parsingTest("use service with underscore", "use service com.aws#Kinesis_2022 hello {}")(
-    "hello".call().useService("com", "aws")("Kinesis_2022")
-  )
-
-  parsingTest("per-operation service reference", "com.aws#Kinesis.hello {}")(
-    Query[Id](
-      useClause = None,
-      operationName = QueryOperationName[Id](
-        QualifiedIdentifier.of("com", "aws", "Kinesis").some,
-        OperationName("hello"),
-      ),
-      struct(),
-    )
-  )
-
-  parsingTest(
-    "per-operation service reference, with whitespace",
-    "com . aws # Kinesis . hello {}",
-  )(
-    Query[Id](
-      useClause = None,
-      operationName = QueryOperationName[Id](
-        QualifiedIdentifier.of("com", "aws", "Kinesis").some,
-        OperationName("hello"),
-      ),
-      struct(),
-    )
-  )
-
-  val simpleResult = "hello".call("world" -> "bar")
-
-  parsingTest("one parameter single-line", """hello { world = "bar" }""")(
-    simpleResult
-  )
-
-  parsingTest("one parameter single-line + trailing comma", """hello { world = "bar", }""")(
-    simpleResult
-  )
-
-  parsingTest(
-    "one parameter multiline",
-    """
-  hello {
-    world = "bar"
   }
-  """,
-  )(simpleResult)
 
-  parsingTest(
-    "one parameter multiline + trailing comma",
-    """
-  hello {
-    world = "bar",
-  }
-  """,
-  )(simpleResult)
-
-  parsingTest(
-    "struct with comment and trailing comma",
-    """
-  hello {
-    world = "bar",
-    //
-  }
-  """,
-  )(
-    "hello".call(
-      "world" -> "bar"
-    )
-  )
-
-  parsingTest(
-    "empty struct",
-    """hello {
-    //hello
-  }""",
-  )("hello".call())
-
-  parsingTest(
-    "one parameter multiline split",
-    """ hello {
-    world
-    =
-    "bar"
-    }""",
-  )(
-    "hello".call(
-      "world" -> "bar"
-    )
-  )
-
-  parsingTest("struct with bool", "hello { verbose = true }")(
-    "hello".call(
-      "verbose" -> true
-    )
-  )
-
-  parsingTest("list with bools", "hello { values = [true, false] }")(
-    "hello".call(
-      "values" -> List(true, false)
-    )
-  )
-
-  parsingTest("list with bools, trailing comma", "hello { values = [true, false,] }")(
-    "hello".call(
-      "values" -> List(true, false)
-    )
-  )
-
-  parsingTest("list with strings", """hello { values = ["hello", "world",] }""")(
-    "hello".call(
-      "values" -> List("hello", "world")
-    )
-  )
-
-  parsingTest("list with ints", """hello { values = [420, 2137,] }""")(
-    "hello".call(
-      "values" -> List(420, 2137)
-    )
-  )
-
-  parsingTest(
-    "list with comments",
-    """
-    hello { values =
-         //before list
-     [
-         //before first elem
-     420 //after first elem
-     ,   //before second elem
-
-      2137 //after second elem,
-           //after trailing comma
-      ]    //after list
-    }""",
-  )(
-    "hello".call(
-      "values" -> List(420, 2137)
-    )
-  )
-
-  parsingTest(
-    "empty comment",
-    "//\nhello{}",
-  )("hello".call())
-
-  parsingTest(
-    "nonempty comment",
-    """hello {
-    // hello
-    }
-    """,
-  )("hello".call())
-
-  parsingTest(
-    "comment inside line",
-    """
-  hello //foo
-    {
-    world = "bar"
-    }
-    """,
-  )(simpleResult)
-
-  parsingTest(
-    "comment at line start",
-    """
-  hello
-  //this is a comment
-  {
-    world = "bar"
-    }
-    """,
-  )(simpleResult)
-
-  parsingTest(
-    "multiple line comments",
-    """
-  hello {
-  //this is a comment
-  //this too
-    world = "bar"
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "more line comments",
-    """
-  hello {
-  //this is a comment
-  world = "bar"
-  //this too
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "comment in the middle of a keypair before line break",
-    """
-  hello {
-  world = //this is a comment
-     "bar"
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "comment in the middle of a keypair after line break",
-    """
-  hello {
-  world =
-  //this is a comment
-     "bar"
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "more comments in the middle of a keypair",
-    """
-  hello {
-  world = //this is a comment
-  //this too
-     "bar"
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "more comments in the middle of a keypair, with preceding whitespace",
-    """
-  hello {
-  world = //this is a comment
-          //this too
-     "bar"
-  }""",
-  )(simpleResult)
-
-  parsingTest(
-    "More complicated case with indentation and comments",
-    """
-CreateHero { //bar
-hero = { //foo
-//    bad = {
-//      evilName = "evil",
-//      powerLevel = 9001,
-//    },
-    good = {
-      howGood = //100
-      //  200,
-      200,
-      anotherKey//foo = "a"
-        = 42,
-    },
-  },
-}
-""",
-  )(
-    "CreateHero".call(
-      "hero" -> struct("good" -> struct("howGood" -> 200, "anotherKey" -> 42))
-    )
-  )
-
-  parsingTest(
-    "Comments literally everywhere",
-    Examples.fullOfComments,
-  )(
-    "op"
-      .call(
-        "firstKey" -> "firstValue",
-        "secondKey" -> "secondValue",
-      )
-      .useService("some", "api")("Service")
-  )
 }
