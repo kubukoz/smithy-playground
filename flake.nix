eae7d8b874744efc2f2d5ef07ee5065d482c92b5
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-31 03:08:44 +0100

    Move library code to ts4s

--- flake.nix
+++ flake.nix
@@ -2,9 +2,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    treesitter4s.url = "github:polyvariant/treesitter4s/native";
+    treesitter4s.inputs.nixpkgs.follows = "nixpkgs";
+    treesitter4s.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, treesitter4s, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -13,46 +16,7 @@
           inherit system;
           overlays = [ jvm ];
         };
-
-        # begin library code
-        rename-grammar = { system, lib, stdenv, libcxxabi, libcxx, grammar, library-name }:
-          stdenv.mkDerivation {
-            inherit (grammar) pname version;
-            src = grammar;
-            buildPhase =
-              if stdenv.isDarwin then ''
-                install_name_tool -id lib${library-name}.dylib parser
-                install_name_tool -change ${libcxxabi}/lib/libc++abi.1.dylib @loader_path/libc++abi.1.dylib parser
-                install_name_tool -change ${libcxx}/lib/libc++.1.0.dylib @loader_path/libc++.1.0.dylib parser
-              '' else "true";
-            installPhase =
-              let suffix = (lib.systems.elaborate system).extensions.sharedLibrary; in
-              ''
-                mkdir -p $out/lib
-                cp parser $out/lib/lib${library-name}${suffix}
-              '';
-          };
-
-        make-grammar-resources =
-          { package
-          , system-mappings ? {
-              "darwin-x86_64" = "x86_64-darwin";
-              "darwin-aarch64" = "aarch64-darwin";
-              "linux-x86_64" = "x86_64-linux";
-              "linux-aarch64" = "aarch64-linux";
-            }
-          }: pkgs.linkFarm "${(package system).name}-all" (pkgs.lib.mapAttrsToList
-            (jna-system: nix-system:
-            let
-              suffix = (pkgs.lib.systems.elaborate nix-system).extensions.sharedLibrary;
-              pkg = package nix-system;
-            in
-            {
-              name = "${jna-system}";
-              path = "${pkg}/lib";
-            })
-            system-mappings);
-        # end library code
+        ts-lib = treesitter4s.lib;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -65,31 +29,31 @@
             pkgs.tree-sitter
           ];
         };
-        packages.grammar-generated = pkgs.stdenv.mkDerivation {
-          pname = "tree-sitter-smithyql-generated";
-          version = "0.0.0";
-          src = ./tree-sitter-smithyql;
-          buildInputs = [ pkgs.tree-sitter pkgs.nodejs ];
-          buildPhase = ''
-            tree-sitter generate
-          '';
-          installPhase = ''
-            cp -r . $out
-          '';
-        };
         packages.grammar =
-          let base = self.packages.${system}.grammar-generated; in
+          let base = pkgs.stdenv.mkDerivation {
+            pname = "tree-sitter-smithyql-generated";
+            version = "0.0.0";
+            src = ./tree-sitter-smithyql;
+            buildInputs = [ pkgs.tree-sitter pkgs.nodejs ];
+            buildPhase = ''
+              tree-sitter generate
+            '';
+            installPhase = ''
+              cp -r . $out
+            '';
+          }; in
           pkgs.callPackage "${nixpkgs}/pkgs/development/tools/parsing/tree-sitter/grammar.nix" { } {
-            language = "smithyql";
+            language = "tree-sitter-smithyql";
             inherit (base) version;
             source = base;
           };
-        packages.grammar-all = make-grammar-resources {
+        packages.grammar-all = pkgs.callPackage ts-lib.make-grammar-resources {
           package = system:
             let pkgs = import nixpkgs { inherit system; }; in
-            pkgs.callPackage rename-grammar {
+            pkgs.callPackage ts-lib.rename-grammar {
+              pname = "tree-sitter-smithyql";
               grammar = self.packages.${system}.grammar;
-              library-name = "tree-sitter-smithyql";
+              rename-dependencies = true;
             };
         };
       }
