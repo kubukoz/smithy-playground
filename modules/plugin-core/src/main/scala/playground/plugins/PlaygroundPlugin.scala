2803d20bf880896bea81adbcf4166e9be6a2824f
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-08 20:12:48 +0100

    More flexible plugin interface

--- modules/plugin-core/src/main/scala/playground/plugins/PlaygroundPlugin.scala
+++ modules/plugin-core/src/main/scala/playground/plugins/PlaygroundPlugin.scala
@@ -1,7 +1,43 @@
 package playground.plugins
 
 import smithy4s.http4s.SimpleProtocolBuilder
+import smithy4s.Service
+import org.http4s.client.Client
+import smithy4s.UnsupportedProtocolError
+import smithy4s.Monadic
+import cats.effect.Concurrent
 
 trait PlaygroundPlugin {
-  def http4sBuilders: List[SimpleProtocolBuilder[_]]
+  @deprecated("Implement simpleBuilders instead", "0.5.2")
+  def http4sBuilders: List[SimpleProtocolBuilder[_]] = Nil
+
+  def simpleBuilders: List[SimpleHttpBuilder] = http4sBuilders.map(
+    SimpleHttpBuilder.fromSimpleProtocolBuilder
+  )
+
+}
+
+/** A more flexible interface for SimpleProtocolBuilder-like things.
+  */
+trait SimpleHttpBuilder {
+
+  def client[Alg[_[_, _, _, _, _]], Op[_, _, _, _, _], F[_]: Concurrent](
+    service: Service[Alg, Op],
+    backend: Client[F],
+  ): Either[UnsupportedProtocolError, Monadic[Alg, F]]
+
+}
+
+object SimpleHttpBuilder {
+
+  def fromSimpleProtocolBuilder(builder: SimpleProtocolBuilder[_]): SimpleHttpBuilder =
+    new SimpleHttpBuilder {
+
+      def client[Alg[_[_, _, _, _, _]], Op[_, _, _, _, _], F[_]: Concurrent](
+        service: Service[Alg, Op],
+        backend: Client[F],
+      ): Either[UnsupportedProtocolError, Monadic[Alg, F]] = builder(service).client(backend).use
+
+    }
+
 }
