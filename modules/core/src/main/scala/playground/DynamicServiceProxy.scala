3965ffda4b681fb62794b8e943dad39f34e49462
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-10 19:56:05 +0100

    Implement DSP precompute

--- modules/core/src/main/scala/playground/DynamicServiceProxy.scala
+++ modules/core/src/main/scala/playground/DynamicServiceProxy.scala
@@ -5,8 +5,8 @@ import cats.implicits._
 import smithy4s.Document
 import smithy4s.Endpoint
 import smithy4s.Service
-import smithy4s.schema.Schema
 import smithy4s.kinds._
+import smithy4s.schema.Schema
 
 class DynamicServiceProxy[Alg[_[_, _, _, _, _]], Op[_, _, _, _, _]](
   service: Service.Aux[Alg, Op]
@@ -73,8 +73,7 @@ class DynamicServiceProxy[Alg[_[_, _, _, _, _]], Op[_, _, _, _, _]](
         def apply[I, E, O, SI, SO](endpoint: Endpoint[Op, I, E, O, SI, SO]): I => F[O] =
           applyWithStatic(endpoint, grp(endpoint.id))
       }
-    // TODO https://github.com/disneystreaming/smithy4s/issues/590
-    // .precompute(service.endpoints)
+        .precomputeBy(service.endpoints, _.name)
 
     new FunctorInterpreter[Op, F] {
       def apply[I, E, O, SI, SO](op: Op[I, E, O, SI, SO]): F[O] = {
@@ -82,6 +81,35 @@ class DynamicServiceProxy[Alg[_[_, _, _, _, _]], Op[_, _, _, _, _]](
         endpointMapping(endpoint)(input)
       }
     }
+  }
+
+  private final implicit class PolyFunction5Ops[F[_, _, _, _, _], G[_, _, _, _, _]](
+    self: PolyFunction5[F, G]
+  ) {
+
+    // copied from smithy4s PolyFunction5's unsafeCacheBy
+    final def precomputeBy[K](
+      allPossibleInputs: Seq[Kind5.Existential[F]],
+      getKey: Kind5.Existential[F] => K,
+    ): PolyFunction5[F, G] =
+      new PolyFunction5[F, G] {
+
+        private val map: Map[K, Any] = {
+          val builder = Map.newBuilder[K, Any]
+          allPossibleInputs.foreach(input =>
+            builder += getKey(input) -> self
+              .apply(input.asInstanceOf[F[Any, Any, Any, Any, Any]])
+              .asInstanceOf[Any]
+          )
+          builder.result()
+        }
+
+        def apply[A0, A1, A2, A3, A4](input: F[A0, A1, A2, A3, A4]): G[A0, A1, A2, A3, A4] = map(
+          getKey(Kind5.existential(input))
+        ).asInstanceOf[G[A0, A1, A2, A3, A4]]
+
+      }
+
   }
 
 }
