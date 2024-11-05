385df93a161e05c0bfbde055f4238bc75ab566bf
Jakub Kozłowski <kubukoz@gmail.com>
2024-11-05 01:42:10 +0100

    Use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
