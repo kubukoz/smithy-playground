58e7f8f8d52be7454e9fde246eaa3930cf127c4c
Jakub Kozłowski <kubukoz@gmail.com>
2024-11-05 01:33:01 +0100

    oops

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
