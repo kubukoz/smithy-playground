c497218bfc236d798d7f0f6026bfe9da837e7ccd
Jakub Kozłowski <kubukoz@gmail.com>
2024-11-09 19:50:24 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -18,10 +18,26 @@
             pkgs.sbt
             pkgs.jless
             pkgs.gnupg
+            (pkgs.tree-sitter.override { webUISupport = true; })
             # temporary, while we don't download coursier ourselves
             pkgs.coursier
           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.xvfb-run ];
         };
+        packages.tree-sitter-smithyql =
+          pkgs.stdenv.mkDerivation {
+            name = "tree-sitter-smithyql";
+            src = ./tree-sitter-smithyql;
+            buildInputs = [pkgs.tree-sitter pkgs.nodejs];
+            buildPhase = ''
+              tree-sitter generate
+              make
+            '';
+            installPhase = if system == "x86_64-darwin" || system == "aarch64-darwin" then ''
+              cp libtree-sitter-smithyql.dylib $out
+            '' else ''
+              cp libtree-sitter-smithyql.so $out
+            '';
+          };
       }
     );
 }
